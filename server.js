2fb45c193d432fb0d32ba9419528e217c10e5dd4
Arijit25865646 <arijitchowdhury734@gmail.com>
2026-09-07 11:16:52 +0530

    cors add

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
 require("dotenv").config();
 
-const cors = require('cors')
-const express = require("express")
+const cors = require("cors");
+const express = require("express");
 
-const connectDB = require("./src/config/db")
-const productRoutes = require("./src/routes/product.routes")
-const userRoutes = require("./src/routes/user.routes")
+const connectDB = require("./src/config/db");
+const productRoutes = require("./src/routes/product.routes");
+const userRoutes = require("./src/routes/user.routes");
 
-const app = express()
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+const app = express();
+app.use(
+  cors({
+    origin: [
+      "https://product-management-frontend-3kfrbug0o-arijit25865646.vercel.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  }),
+);
 connectDB();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/", (req, res)=>{
-    res.send("Welcome to my server")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to my server");
+});
 
 app.use("/products", productRoutes);
 app.use("/auth", userRoutes);
 
-
-app.listen(5000, ()=>{
-    console.log("server running....")
-})
+app.listen(5000, () => {
+  console.log("server running....");
+});
